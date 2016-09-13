19a11566448831ff9250baaef6eccfb739619871
Oscar Llumiquinga <davichochaos@hotmail.com>
2016-09-13 14:29:35 -0500

    sitio

---
+++ scorts/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<meta charset="utf-8">
+<head>
+	<title></title>
+</head>
+<body>
+	<h1>¿Eres mayor de 18 años?</h1>
+	<button>Si</button>
+	<button>No</button>
+</body>
+</html>
